f22d9b0e4ba4015cbbf5fc2f65523dfbe57571a9
Amrit <amrit.madhav.srivastava@gmail.com>
2026-02-09 18:18:10 -0500

    notes on flow

--- main.py
+++ main.py
@@ -29,6 +29,50 @@ def fill_resident_form():
         # resident email label
         #
         # resident neighborhood drop down menu -> east
+        #
+        # next
+        #
+        # community drop down -> akers
+        #
+        # building drop down -> west akers
+        #
+        # room number drop down with search bar
+        #
+        # next
+        #
+        # corresponding interaction -> 3
+        #
+        # was it successful? -> drop down bool
+        #
+        # next
+        #
+        # if it was successful:
+        #
+        ## candid conversation date -> calendar type deal
+        ##
+        ## 2/3 sentence summary
+        ##
+        ## keywords selection
+        ##
+        ## briefly summarize information that was shared that would warrant follow up
+        ##
+        ## next
+        #
+        # if it wasn't successful
+        #
+        # how many times did you attempt to engage? 1/2 to 3/4 or more
+        #
+        # list the dates you attempted to engage with the resident - multiple date objects
+        #
+        # which strategies did you use - drop down for knocked on door, text message, email, social media or messaging app
+        #
+        # conversation challenges - multi box for resident declined or avoided interaction, resident seemed disinterested or disengaged, scheduling conflict, resident was unavailable
+        #
+        # provide a narrative summary of your efforts and challenges to complete the conversation
+        #
+        # what do you plan to do differently to increase the changes of a successful conversation for future candid conversations
+        #
+        # next to submit
 
         print("Form submitted successfully!")
         browser.close()
